7a3e34e2609df9f56cab98ce011da974977cd09c
w-yakbah <w.yakbah@outlook.com>
2022-07-16 14:31:36 +0200

    Update Enemy.gd

--- bullet/Enemy.gd
+++ bullet/Enemy.gd
@@ -1,32 +1,39 @@
 extends Node2D
 
 const bullet_scene = preload("res://bullet/Bullet.tscn")
+extends Node2D
+
+const bullet_scene = preload("res://Bullet.tscn")
 onready var shoot_timer = $ShootTimer
 onready var rotater = $Rotater
 
 const rotate_speed = 100
-const shooter_timer_wait_time = 0.2
+const shoot_timer_wait_time = 0.2
 const spawn_point_count = 4
 const radius = 100
 
-func _ready():
-		var step = 2*3.14 / spawn_point_count
+func _ready():	
+	var step = 2 * PI / spawn_point_count
+	
+	for i in range(spawn_point_count):
+		var spawn_point = Node2D.new()
+		var pos = Vector2(radius, 0).rotated(step * i)
+		spawn_point.position = pos
+		spawn_point.rotation = pos.angle()
+		rotater.add_child(spawn_point)
 		
-		for i in range (spawn_point_count):
-			var spawn_point = Sprite.new()
-			var pos = Vector2(radius,0).rotated(step * 1)
-			spawn_point.position = pos
-			spawn_point.rotation = pos.angle()
-		shoot_timer.wait_time = shooter_timer_wait_time
-		shoot_timer.start() 
+	shoot_timer.wait_time = shoot_timer_wait_time
+	shoot_timer.start()
 
-func _process(delta: float)-> void:
+
+func _process(delta):
 	var new_rotation = rotater.rotation_degrees + rotate_speed * delta
-	rotater.rotation_degrees= fmod(new_rotation,360)
+	rotater.rotation_degrees = fmod(new_rotation, 360)
+	
 
-func _on_ShootTimer_timeout()-> void:
+func _on_ShootTimer_timeout() -> void:
 	for s in rotater.get_children():
 		var bullet = bullet_scene.instance()
 		get_tree().root.add_child(bullet)
-		bullet.position =s.global_position
-		bullet.rotation =s.global_rotation
+		bullet.position = s.global_position
+		bullet.rotation = s.global_rotation	
